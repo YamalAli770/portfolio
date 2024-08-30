554d3f1e10789281251452b7b82c4261ed5b92f9
Yamal Ali <yamalali770@gmail.com>
2024-08-30 21:52:48 +0500

    made work page responsive

--- src/components/work-grid.tsx/project-card.tsx
+++ src/components/work-grid.tsx/project-card.tsx
@@ -11,16 +11,16 @@ export interface ProjectCardProps {
 
 const ProjectCard = ({ img, category, name, href }: ProjectCardProps) => {
   return (
-    <Card className=" bg-card-bg-dark border border-gray-800 rounded-3xl col-span-2">
+    <Card className="lg:col-span-2 md:col-span-3 col-span-6 bg-card-bg-dark border border-gray-800 rounded-3xl">
         <CardHeader>
-            <img src={img} alt={name} className="rounded-3xl w-full h-56 object-cover" />
+            <img src={img} alt={name} className="rounded-3xl w-full h-56 object-cover object-top" />
         </CardHeader>
         <CardContent className="flex justify-between items-center">
             <div className="flex flex-col gap-1 items-start">
                 <h3 className="text-gray-500 uppercase text-sm">{category}</h3>
                 <h1 className="text-white font-medium text-xl font-acorn">{name}</h1>
             </div>
-            <Link to={href}>
+            <Link to={href} target="__blank" referrerPolicy={"no-referrer"}>
                 <CardLinkIcon />
             </Link>
         </CardContent>
