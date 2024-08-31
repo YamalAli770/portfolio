c8493fd36abaeb87048769b28b45092494c6fee2
Yamal Ali <yamalali770@gmail.com>
2024-08-31 22:19:08 +0500

    fixed tech stack image paths

--- src/components/about-grid/tech-stack.tsx
+++ src/components/about-grid/tech-stack.tsx
@@ -4,51 +4,51 @@ import TechItem, { TechItemProps } from "./tech-item";
 const techItems: TechItemProps[] = [
   {
     name: "JavaScript",
-    logo: "src/assets/tech/js.png"
+    logo: "assets/tech/js.png"
   },
   {
     name: "TypeScript",
-    logo: "src/assets/tech/ts.png"
+    logo: "assets/tech/ts.png"
   },
   {
     name: "TailwindCSS",
-    logo: "src/assets/tech/tailwind.png"
+    logo: "assets/tech/tailwind.png"
   },
   {
     name: "ReactJS",
-    logo: "src/assets/tech/react.png"
+    logo: "assets/tech/react.png"
   },
   {
     name: "Redux",
-    logo: "src/assets/tech/redux.png"
+    logo: "assets/tech/redux.png"
   },
   {
     name: "NextJS",
-    logo: "src/assets/tech/next.png"
+    logo: "assets/tech/next.png"
   },
   {
     name: "NodeJS",
-    logo: "src/assets/tech/node.png"
+    logo: "assets/tech/node.png"
   },
   {
     name: "NestJS",
-    logo: "src/assets/tech/nest.png"
+    logo: "/assets/tech/nest.png"
   },
   {
     name: "MongoDB",
-    logo: "src/assets/tech/mongodb.png"
+    logo: "assets/tech/mongodb.png"
   },
   {
     name: "PostgreSQL",
-    logo: "src/assets/tech/postgres.png"
+    logo: "assets/tech/postgres.png"
   },
   {
     name: "Firebase",
-    logo: "src/assets/tech/firebase.png"
+    logo: "assets/tech/firebase.png"
   },
   {
     name: "Git",
-    logo: "src/assets/tech/git.png"
+    logo: "assets/tech/git.png"
   }
 ];
 
